751889130bf561e647cb31c35c4f89ac40f0dd68
Jacob Schaider <jacobschaider@Jeffs-MacBook-Pro.local>
2018-12-05 13:41:35 -0800

    Updated all UI of app

--- app/src/main/java/com/brunogtavares/minglr/ChooseLoginRegistrationActivity.java
+++ app/src/main/java/com/brunogtavares/minglr/ChooseLoginRegistrationActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.support.v7.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
